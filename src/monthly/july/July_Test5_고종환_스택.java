05871e8f13e49bc982e0c34bba9f8af12018c6e0
Jonghwan Ko <0106koj@gmail.com>
2025-08-01 13:30:45 +0900

    July_Test5 풀이 개선

--- src/monthly/july/July_Test5_고종환_스택.java
+++ src/monthly/july/July_Test5_고종환_스택.java
@@ -13,31 +13,26 @@ public class July_Test5_고종환_스택 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int[] arr = new int[N];
-		int[][] dp = new int[N][2]; // height, idx
-
 		int[] ans = new int[N];
 		int[] reversedAns = new int[N];
 
-		int[][] nearest = new int[N][2]; // {idx, length}
-		int[][] reversedNearest = new int[N][2];
+		int[] nearest = new int[N];
+		int[] reversedNearest = new int[N];
+		int[] reversedArr = new int[N];
 
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int[] reversedArr = new int[N];
-		for (int i = 0; i < N; i++) {
-			reversedArr[i] = arr[N - 1 - i];
+			reversedArr[N - 1 - i] = arr[i];
 		}
 
 		// left to right
-		solution(N, dp, ans, nearest, arr);
+		solution(N, ans, nearest, arr);
 
 		// right to left
-		solution(N, dp, reversedAns, reversedNearest, reversedArr);
+		solution(N, reversedAns, reversedNearest, reversedArr);
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < N; i++) {
@@ -47,33 +42,38 @@ public class July_Test5_고종환_스택 {
 				sb.append("0\n");
 			} else {
 				// select nearest building
-				int nearestIdx = nearest[i][1] <= reversedNearest[reversedIdx][1] ? nearest[i][0] : N - 1 - reversedNearest[reversedIdx][0];
+				int leftDist = nearest[i] != -1 ? i - nearest[i] : Integer.MAX_VALUE;
+				int rightDist = reversedNearest[reversedIdx] != -1 ? reversedIdx - reversedNearest[reversedIdx] : Integer.MAX_VALUE;
+
+				int nearestIdx;
+				if (leftDist <= rightDist) {
+					nearestIdx = nearest[i];
+				} else {
+					nearestIdx = N - 1 - reversedNearest[reversedIdx];
+				}
 				sb.append(total).append(" ").append(nearestIdx + 1).append("\n");
 			}
 		}
 		System.out.println(sb);
 	}
 
-	private static void solution(int n, int[][] dp, int[] ans, int[][] nearest, int[] arr) {
+	private static void solution(int n, int[] ans, int[] nearest, int[] arr) {
 		int size = 1;
-		dp[0][0] = arr[0];
-		dp[0][1] = 0;
-		nearest[0][1] = Integer.MAX_VALUE;
+		int[] stack = new int[n];
+		stack[0] = 0;
+		nearest[0] = -1;
 		for (int i = 1; i < n; i++) {
-			while (size > 0 && dp[size - 1][0] <= arr[i]) {
+			while (size > 0 && arr[stack[size - 1]] <= arr[i]) {
 				size--; // pop
 			}
 			// push
-			dp[size][0] = arr[i];
-			dp[size][1] = i;
-			ans[i] += size;
-			if (size >= 1) {
-				nearest[i][0] = dp[size - 1][1];
-				nearest[i][1] = i - dp[size - 1][1];
+			stack[size++] = i;
+			ans[i] += size - 1;
+			if (size >= 2) {
+				nearest[i] = stack[size - 2];
 			} else {
-				nearest[i][1] = Integer.MAX_VALUE;
+				nearest[i] = -1;
 			}
-			size++;
 		}
 	}
 }
