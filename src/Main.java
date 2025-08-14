969f7c688cb586c682b73ff40e247071bc371b3c
0106koj <jonghwankoh@ajou.ac.kr>
2025-08-15 06:00:39 +0900

    reset template file

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
-/*
- * BJ S1 1325. 효율적인 해킹
- * 메모:
- * visit 체크를 큐에 넣는 순간 해야 시간초과나지 않는다.
- * 
-**/
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.Arrays;
-=======
->>>>>>> f1ef2467fb38ffc559857edd626babd8c9a0366b
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -23,65 +14,6 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-<<<<<<< HEAD
 		
 	}
 }
-=======
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-
-		graph = new ArrayList[n+1];
-		for (int i = 0; i <= n; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		visited = new int[n + 1];
-
-		while (m-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
-			graph[b].add(a);
-		}
-
-		int[] virusCnt = new int[n + 1];
-		int maxCnt = 0;
-		for (int i = 1; i <= n; i++) {
-			int cnt = bfs(i);
-			virusCnt[i] = cnt;
-			maxCnt = Math.max(maxCnt, cnt);
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= n; i++) {
-			if (virusCnt[i] == maxCnt) {
-				sb.append(i).append(" ");
-			}
-		}
-		System.out.println(sb);
-	}
-
-	private static int bfs(int start) {
-		int cnt = 1;
-
-		queue.offer(start);
-		while (!queue.isEmpty()) {
-			int cur = queue.poll();
-			
-            if (visited[cur] == start) {
-                continue;
-            }
-            visited[cur] = start;
-            cnt++;
-			for (int next : graph[cur]) {
-				if (visited[next] != start) {
-					queue.offer(next);
-				}
-			}
-		}
-
-		return cnt;
-	}
-}
->>>>>>> f1ef2467fb38ffc559857edd626babd8c9a0366b
--- src/Solution.java
+++ src/Solution.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-=======
->>>>>>> f1ef2467fb38ffc559857edd626babd8c9a0366b
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
 class Solution
 {
-<<<<<<< HEAD
     private static int N;
     private static int[] cardList;
     private static int[] opsCnt;
@@ -19,116 +10,8 @@ class Solution
 
     public static void main(String args[]) throws Exception
     {
-        System.setIn(new FileInputStream("res/input.txt"));
+        // System.setIn(new FileInputStream("res/input.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            N = Integer.parseInt(br.readLine());
-            opsCnt = new int[4];
-            cardList = new int[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            for (int i = 0; i < 4; i++) {
-                opsCnt[i] = Integer.parseInt(st.nextToken());
-            }
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                cardList[i] = Integer.parseInt(st.nextToken());
-            }
-
-            maxAns = Integer.MIN_VALUE;
-            minAns = Integer.MAX_VALUE;
-            solution(0, cardList[0]);
-            sb.append("#" + test_case + " " + (maxAns - minAns) + "\n");
-        }
-        System.out.println(sb);
-
-
-    }
-
-    private static void solution(int size, int result) {
-        if (size == N - 1) {
-            maxAns = Math.max(maxAns, result);
-            minAns = Math.min(minAns, result);
-            return;
-        }
-        for (int i = 0; i < 4; i++) {
-            if (opsCnt[i] > 0) {
-                opsCnt[i]--;
-                int next = result;
-                switch (i) {
-                    case 0:
-                        next += cardList[size + 1];
-                        break;
-                    case 1:
-                        next -= cardList[size + 1];
-                        break;
-                    case 2:
-                        next *= cardList[size + 1];
-                        break;
-                    case 3:
-                        next /= cardList[size + 1];
-                        break;
-                }
-                solution(size + 1, next);
-                opsCnt[i]++;
-            }
-        }
-=======
-    static int N, ans;
-    static int[] graph;
-	public static void main(String args[]) throws Exception
-	{
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            graph = new int[N + 1]; // graph[i] -> 인덱스 i 이상의 재료 중 조합 불가능한 재료 집합
-            for (int i = 1; i <= N; i++) {
-                graph[i] = 1 << i;
-            }
-
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                if (b > a) {
-                    graph[a] |= 1 << b;
-                } else {
-                    graph[b] |= 1 << a;
-                }
-            }
-            ans = 0;
-			combination(0, 0);
-            sb.append("#").append(test_case).append(' ').append(ans).append('\n');
-		}
-        System.out.println(sb);
-	}
-
-    static void combination(int depth, int blackList) {
-        if (depth == N) {
-            ans++;
-            return;
-        }
-
-        combination(depth + 1, blackList);
-        if ((blackList & (1 << (depth + 1))) == 0) {
-            combination(depth + 1, blackList | graph[depth + 1]);
-        }
-
->>>>>>> f1ef2467fb38ffc559857edd626babd8c9a0366b
     }
 }
